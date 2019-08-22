b01c24d8b6539f91cf264f1fe755a2cf51353e8b
Shrikant Tonape <shrikant.tonape@weboniselab.com>
2019-08-22 18:47:13 +0530

    Update swagger configuration

--- src/main/java/com/webonise/login/controller/LoginController.java
+++ src/main/java/com/webonise/login/controller/LoginController.java
@@ -1,28 +1,30 @@
 package com.webonise.login.controller;
 
-import com.webonise.login.model.LoginRequest;
-import com.webonise.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webonise.login.model.LoginRequest;
+import com.webonise.login.service.UserService;
+
 @RestController
+@RequestMapping("/")
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
-    public String login () {
+    @GetMapping
+    public String login() {
         return "Welcome to login App";
-
     }
 
     @PostMapping("/login")
-    public boolean login (@RequestBody LoginRequest loginRequest) {
+    public boolean login(@RequestBody LoginRequest loginRequest) {
         return userService.login(loginRequest.getLoginId(), loginRequest.getPassword());
-
     }
+    
 }
--- src/main/java/com/webonise/login/controller/SignUpController.java
+++ src/main/java/com/webonise/login/controller/SignUpController.java
@@ -6,9 +6,11 @@ import com.webonise.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
+@RequestMapping("/")
 public class SignUpController {
 
     @Autowired
