6fe369eb0a497ea4c42e7f2c883589b67647480e
Sahadev Bite <sahadevbite@gmail.com>
2019-12-18 17:26:00 +0530

    it will give unique result

--- src/main/java/com/webonise/login/dao/UserEntityDao.java
+++ src/main/java/com/webonise/login/dao/UserEntityDao.java
@@ -3,11 +3,13 @@ package com.webonise.login.dao;
 import com.webonise.login.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 @Repository
 public interface UserEntityDao extends JpaRepository<UserEntity, Long> {
-
+    
+    @Query(value = "SELECT a FROM UserEntity a WHERE loginId = ?1 AND password = ?2 GROUP BY loginId")
     UserEntity findByLoginIdAndPassword(String loginId, String password);
 }
