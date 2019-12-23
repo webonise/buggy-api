fe369d619b6c5a89ba6e399d3ac5b9af1e1c1ef9
Sahadev Bite <sahadevbite@gmail.com>
2019-12-23 14:07:06 +0530

    db and controller changes

--- src/main/java/com/webonise/login/controller/SignUpController.java
+++ src/main/java/com/webonise/login/controller/SignUpController.java
@@ -1,5 +1,7 @@
 package com.webonise.login.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webonise.login.model.UserDTO;
+import com.webonise.login.model.UserEntity;
 import com.webonise.login.model.UserRequest;
 import com.webonise.login.service.UserService;
 
 @RestController("/user")
 @RequestMapping("/")
 public class SignUpController {
+	
+	static Log logger = LogFactory.getLog(SignUpController.class);
 
     @Autowired
     private UserService userService;
@@ -20,10 +25,10 @@ public class SignUpController {
     @PostMapping
     public UserDTO saveUser(@RequestBody UserRequest userRequest) {
     	int check = userService.checkUser(userRequest.getLoginId());
-    	System.err.println(check);
+    	logger.info(check);
     	if(check>0) {
-    		
-    		return null;
+    		UserDTO data = new UserDTO(new UserEntity());
+    		return data;
     		
     	}else {
     		
