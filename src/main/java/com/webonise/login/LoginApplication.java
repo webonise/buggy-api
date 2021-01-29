0a107e1f055283959df101fd96ab79df5fae5747
unknown <ashishcharan001@gmail.com>
2021-01-29 12:07:03 -0800

    Add unique constraint & loggers && removed duplicate for insert

--- src/main/java/com/webonise/login/LoginApplication.java
+++ src/main/java/com/webonise/login/LoginApplication.java
@@ -1,5 +1,6 @@
 package com.webonise.login;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -9,7 +10,11 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class LoginApplication {
 
+	private static final Logger logger = Logger.getLogger(LoginApplication.class);
 	public static void main(String[] args) {
+		 if(logger.isDebugEnabled()){
+	            logger.debug("$$$$$$$ LoginApplication is executed! $$$$$$$$");
+	        }
 		SpringApplication.run(LoginApplication.class, args);
 	}
 }
--- src/main/java/com/webonise/login/service/impl/UserServiceImpl.java
+++ src/main/java/com/webonise/login/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.webonise.login.service.impl;
 
+import com.webonise.login.controller.LoginController;
 import com.webonise.login.dao.UserEntityDao;
 import com.webonise.login.model.UserDTO;
 import com.webonise.login.model.UserRequest;
 import com.webonise.login.model.UserEntity;
 import com.webonise.login.service.UserService;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +19,28 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserEntityDao userEntityDao;
 
+	private static final Logger logger = Logger.getLogger(UserServiceImpl.class);
+	
     @Override
     public UserDTO saveUser(UserRequest userRequest) {
-        UserEntity userEntity = userEntityDao.save(new UserEntity(userRequest));
-        return new UserDTO(userEntity);
+    UserEntity userEntity =null;
+    try
+    {
+    	 logger.debug("Saving in DB..............");
+         userEntity = userEntityDao.save(new UserEntity(userRequest));
+         
+    }
+    catch(Exception e)
+    {
+	   //can use logger here
+    	 logger.error("%%%%%%%%%"+e.getMessage());
+    }
+    return new UserDTO(userEntity);
     }
 
     @Override
     public boolean login(String loginId, String password) {
+    	logger.debug("Login in  with username & password..............");
         UserEntity user = userEntityDao.findByLoginIdAndPassword(loginId, password);
         return user != null;
     }
